47cc1fa8bd69cfb8a44c7d5fa901e4a9aa9993fc
xueh3 <xueh3@ke.com>
2023-12-31 20:05:29 +0800

    循环队列

--- algorithm/Queue/base.go
+++ algorithm/Queue/base.go
@@ -1,61 +1,74 @@
 package Queue
 
-import (
-	"fmt"
-)
+import "fmt"
 
 //队列相关学习
 type Queue struct {
 }
 
 func (q *Queue) Run() (err error) {
-	qa := new(QueueArray)
-	qa.MaxSize = 4
-	qa.QueueElement = make([]int, qa.MaxSize, qa.MaxSize)
+	//循环队列学习
 
-	_, err = qa.Push(1)
-	fmt.Println("队列当前长度为", qa.GetSize())
-	qa.Traverse()
-	//弹出队列
-	ele1, err := qa.Pop()
-	if err != nil {
-		return err
-	}
-	fmt.Println("弹出元素为", ele1, "队列当前长度为", qa.GetSize())
-	qa.Traverse()
-	_, err = qa.Push(2)
-	fmt.Println("队列当前长度为", qa.GetSize())
-	qa.Traverse()
-	//弹出队列
-	ele2, err := qa.Pop()
-	if err != nil {
-		return err
-	}
-	fmt.Println("弹出元素为", ele2, "队列当前长度为", qa.GetSize())
-	qa.Traverse()
+	obj := Constructor(3) //初始化队列长度
+	fmt.Println(obj.EnQueue(1))
+	fmt.Println(obj.EnQueue(2))
+	fmt.Println(obj.EnQueue(3))
+	fmt.Println(obj.EnQueue(4))
+	fmt.Println(obj.Rear())
+	fmt.Println(obj.IsFull())
+	fmt.Println(obj.DeQueue())
+	fmt.Println(obj.EnQueue(4))
+	fmt.Println(obj.Rear())
 
-	_, err = qa.Push(3)
-	fmt.Println("队列当前长度为", qa.GetSize())
-	qa.Traverse()
-	_, err = qa.Push(4)
-	fmt.Println("队列当前长度为", qa.GetSize())
-	qa.Traverse()
-	if err != nil {
-		return err
-	}
+	//普通队列
+	/*
+		qa := new(QueueArray)
+		qa.MaxSize = 4
+		qa.QueueElement = make([]int, qa.MaxSize, qa.MaxSize)
 
-	ele3, err := qa.Pop()
-	if err != nil {
-		return err
-	}
-	fmt.Println("弹出元素为", ele3, "队列当前长度为", qa.GetSize())
-	qa.Traverse()
-	ele4, err := qa.Pop()
-	if err != nil {
-		return err
-	}
-	fmt.Println("弹出元素为", ele4, "队列当前长度为", qa.GetSize())
-	qa.Traverse()
+		_, err = qa.Push(1)
+		fmt.Println("队列当前长度为", qa.GetSize())
+		qa.Traverse()
+		//弹出队列
+		ele1, err := qa.Pop()
+		if err != nil {
+			return err
+		}
+		fmt.Println("弹出元素为", ele1, "队列当前长度为", qa.GetSize())
+		qa.Traverse()
+		_, err = qa.Push(2)
+		fmt.Println("队列当前长度为", qa.GetSize())
+		qa.Traverse()
+		//弹出队列
+		ele2, err := qa.Pop()
+		if err != nil {
+			return err
+		}
+		fmt.Println("弹出元素为", ele2, "队列当前长度为", qa.GetSize())
+		qa.Traverse()
 
+		_, err = qa.Push(3)
+		fmt.Println("队列当前长度为", qa.GetSize())
+		qa.Traverse()
+		_, err = qa.Push(4)
+		fmt.Println("队列当前长度为", qa.GetSize())
+		qa.Traverse()
+		if err != nil {
+			return err
+		}
+
+		ele3, err := qa.Pop()
+		if err != nil {
+			return err
+		}
+		fmt.Println("弹出元素为", ele3, "队列当前长度为", qa.GetSize())
+		qa.Traverse()
+		ele4, err := qa.Pop()
+		if err != nil {
+			return err
+		}
+		fmt.Println("弹出元素为", ele4, "队列当前长度为", qa.GetSize())
+		qa.Traverse()
+	*/
 	return
 }
---
+++ algorithm/Queue/circularQueue.go
@@ -0,0 +1,89 @@
+package Queue
+
+//循环队列
+type MyCircularQueue struct {
+	Size     int //用来计算是否溢出
+	Left     int
+	Right    int
+	QueueEle []int
+	Limit    int //总长度
+}
+
+func Constructor(k int) MyCircularQueue {
+	return MyCircularQueue{
+		Limit:    k,
+		QueueEle: make([]int, k, k),
+	}
+}
+
+//写入队列元素
+func (this *MyCircularQueue) EnQueue(value int) bool {
+	//判断是否溢出
+	if (this.Size + 1) > this.Limit { //溢出了直接返回false
+		return false
+	}
+	this.Size++
+	this.QueueEle[this.Right] = value //给队列赋值
+	if this.Right+1 > this.Limit-1 {  //超出界限从头计算
+		this.Right = 0
+	} else {
+		this.Right++
+	}
+	return true
+}
+
+//弹出队列元素
+func (this *MyCircularQueue) DeQueue() bool {
+	if this.IsEmpty() { //空队列 无法弹出
+		return false
+	}
+	if (this.Left + 1) == this.Limit {
+		this.Left = 0
+	} else {
+		this.Left++
+	}
+	this.Size-- //长度-1
+	return true
+}
+
+//返回队列第一个元素
+func (this *MyCircularQueue) Front() int {
+	if this.IsEmpty() {
+		return -1
+	}
+	//根据left计算下标
+	//var index int
+	//
+	//if this.Left==0
+	//index = this.Left - 1
+	return this.QueueEle[this.Left]
+}
+
+//弹出尾部元素
+func (this *MyCircularQueue) Rear() int {
+	if this.IsEmpty() {
+		return -1
+	}
+	var index int
+	if this.Right == 0 {
+		index = this.Limit - 1
+	} else {
+		index = this.Right - 1
+	}
+
+	return this.QueueEle[index]
+}
+
+func (this *MyCircularQueue) IsEmpty() bool {
+	if this.Size == 0 {
+		return true
+	}
+	return false
+}
+
+func (this *MyCircularQueue) IsFull() bool {
+	if this.Size == this.Limit {
+		return true
+	}
+	return false
+}
